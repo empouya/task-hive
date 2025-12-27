69be48263286bb2558ebc00d0ce8e55e2dc09197
Eid Mohammad Pouya <empouya03@gmail.com>
2025-12-27 10:58:16 +0100

    feature: project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from teams.models import Team
 
-# Create your models here.
+class Project(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = 'ACTIVE', 'Active'
+        ARCHIVED = 'ARCHIVED', 'Archived'
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='projects')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=10, 
+        choices=Status.choices, 
+        default=Status.ACTIVE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.team.name} | {self.name}"
