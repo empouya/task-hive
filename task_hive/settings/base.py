bfee1580936a47c114965494cc141b0c454c891e
Eid Mohammad Pouya <empouya03@gmail.com>
2025-12-31 12:04:05 +0100

    fix: setting up cors headers to backend

--- task_hive/settings/base.py
+++ task_hive/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'notifications',
 ]
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- task_hive/settings/dev.py
+++ task_hive/settings/dev.py
@@ -3,7 +3,15 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+CORS_ALLOW_CREDENTIALS = True
+
 
 
 # Database
